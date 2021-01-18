c193609118f3b6faa4d23d4c0b7382d8c6c5b51c
ShoroveTajmen <sorovitajmen@gmail.com>
2021-01-18 06:53:23 +0600

    find happy number using hashset

---
+++ src/leetcode202/HappyNumber.java
@@ -0,0 +1,35 @@
+package leetcode202;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class HappyNumber {
+    public static  boolean isHappy(int n) {
+        Set<Integer> numberSet = new HashSet<>();
+        while (n != 1){
+            int current = n;
+            int sum = 0;
+            while(current != 0){
+                int rem = current%10;
+                sum = sum+(rem*rem);
+                current = current/10;
+            }
+            if (numberSet.contains(sum)){
+                return false;
+            }
+            n = sum;
+            numberSet.add(n);
+        }
+        return  true;
+    }
+    public static void main(String[] args) {
+        HappyNumber h = new HappyNumber();
+        int n=19;
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        n = s1.nextInt();
+        System.out.println(h.isHappy(n));
+
+    }
+}
