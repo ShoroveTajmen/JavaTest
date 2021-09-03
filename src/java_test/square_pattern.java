6feea1af81d39e66a07b29c4294592aa04b36fb4
ShoroveTajmen <sorovitajmen@gmail.com>
2021-09-03 10:03:10 +0600

    square pattern

---
+++ src/java_test/square_pattern.java
@@ -0,0 +1,12 @@
+package java_test;
+
+public class square_pattern {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            System.out.println("\n");
+            for(int j = 0; j<5; j++){
+                System.out.print("* ");
+            }
+        }
+    }
+}
